8282423dd3a0aedc5601c9bcbb2b9990cbe71aee
Barsonica <44613217+Barsonica@users.noreply.github.com>
2019-01-22 11:18:40 +0100

    version 1.1

--- Barsonica Quill/AboutForm.Designer.cs
+++ Barsonica Quill/AboutForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Barsonica_Quill
+namespace Barsonica_Quill
 {
     partial class AboutForm
     {
@@ -44,7 +44,7 @@
             // 
             this.pictureBox1.BackgroundImage = global::Barsonica_Quill.Properties.Resources.Copyleft;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(253, 48);
+            this.pictureBox1.Location = new System.Drawing.Point(277, 43);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(50, 50);
             this.pictureBox1.TabIndex = 0;
@@ -53,78 +53,78 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Liberation Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(54, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 36);
+            this.label1.Size = new System.Drawing.Size(233, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "Barsonica Quill";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Liberation Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(68, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 21);
+            this.label2.Size = new System.Drawing.Size(80, 24);
             this.label2.TabIndex = 2;
             this.label2.Text = "Version:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Liberation Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.Location = new System.Drawing.Point(190, 45);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 21);
+            this.label3.Size = new System.Drawing.Size(35, 24);
             this.label3.TabIndex = 3;
-            this.label3.Text = "1.0";
+            this.label3.Text = "1.1";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Liberation Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.Location = new System.Drawing.Point(190, 69);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 21);
+            this.label4.Size = new System.Drawing.Size(40, 24);
             this.label4.TabIndex = 5;
             this.label4.Text = "x86";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Liberation Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label5.Location = new System.Drawing.Point(68, 69);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(116, 21);
+            this.label5.Size = new System.Drawing.Size(116, 24);
             this.label5.TabIndex = 4;
             this.label5.Text = "Architecture:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Liberation Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label6.Location = new System.Drawing.Point(25, 101);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(264, 21);
+            this.label6.Size = new System.Drawing.Size(262, 24);
             this.label6.TabIndex = 6;
             this.label6.Text = "Distributed under MIT Licence";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Liberation Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label7.Location = new System.Drawing.Point(7, 125);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(298, 17);
+            this.label7.Size = new System.Drawing.Size(344, 18);
             this.label7.TabIndex = 7;
-            this.label7.Text = "https://github.com/Barsonica/Barsonica-Quill";
+            this.label7.Text = "https://github.com/Barsonica/BarsonicaQuillCsharp";
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 150);
+            this.ClientSize = new System.Drawing.Size(350, 150);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
